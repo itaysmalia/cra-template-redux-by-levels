88428fdb5c410dbadd9c8581d8c12ac0f53dad10
Itay Sin-Malia <itay.sinmalia@brightmachines.com>
2020-12-06 22:41:27 +0200

    fix components imports

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Provider } from "react-redux";
 import { RBLProvider } from "redux-by-levels";
 
-import store from './store/store'
-import Count from './count'
-import Name from './name'
+import store from "./store/store";
+import Count from "./components/count/Count";
+import Name from "./components/name/Name";
 import Both from "./components/both/Both";
 
 const App = () => {
